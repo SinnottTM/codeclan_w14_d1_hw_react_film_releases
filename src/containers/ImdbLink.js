a2f32fd6cbc10a95e6bab5bffe35afd49e78aaf7
SinnottTM <tim.sinnott86@gmail.com>
2021-05-24 23:54:03 +0100

    Second Commit - setting up containers

---
+++ src/containers/ImdbLink.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ImdbLink = () => {
+
+    return (
+
+        <>
+            <h3><a href="https://www.imdb.com/calendar/?region=gb" target="blank">View more upcoming releases</a></h3>
+
+        </>
+    );
+
+}
+
+export default ImdbLink;
